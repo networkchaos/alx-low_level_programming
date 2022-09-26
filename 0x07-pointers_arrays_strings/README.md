dedc88046f9a4c4ab4ce44a8cbc8dce05a9d6108
jijo <jojogrape850@gmail.com>
2022-09-26 08:02:51 -0700

    main.h

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+alx 0x07-pointers_arrays_strings
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+[A[A
