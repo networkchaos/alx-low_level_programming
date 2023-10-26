3b1008df22122672a9ca768969daa55eafc37ced
@networkchaos <65135432+networkchaos@users.noreply.github.com>
2023-10-26 12:45:36 +0300

    recursion

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+alx_low_level_programming 0x08-recursion project
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,18 @@
+void _puts_recursion(char *s);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+void _putchar(char c);
+int _strlen(char *str);
+char *move_past_star(char *s2);
+int inception(char *s1, char *s2);
+int _strlen(char *s);
+int do_is_palindrome(char *s, int l, int i);
+int do_is_prime_number(int n, int m);
+int do_sqrt_recursion(int n, int r);
